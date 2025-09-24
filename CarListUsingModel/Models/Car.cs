249e4a31f31175e56b2b8753812dd53dd93adae0
Lance Kent <lancemagollado46@gmail.com>
2025-09-24 17:21:53 +0800

    Adding Models

---
+++ CarListUsingModel/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarListUsingModel.Models
+{
+    public class Car
+    {
+        public int carID {  get; set; }
+        public string carBrand { get; set; }
+        public string carDescription { get; set; }
+        public string carModel { get; set; }
+        public int carPrice { get; set; }
+    }
+}
